bbeeb342421c09c9097393d8b7ffc8e32e3bac20
srodriguezsu <srodriguezsu@unal.edu.co>
2023-08-22 11:38:00 -0500

    First commit in the repo

---
+++ actividad_1/ejercicio_4/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int edJuan, edAlber, edAna, edMama ;
+        System.out.print("La edad de Juan es: ");
+        edJuan = input.nextInt();
+        edAlber = 2 * edJuan / 3;
+        edAna = 4 * edJuan / 3;
+        edMama = edJuan + edAlber + edAna;
+        System.out.println("Las edades son: \n Alberto = " + edAlber + "\n Juan = " + edJuan + "\n Ana = " + edAna + "\n Mamá = " + edMama);
+    }
+}
