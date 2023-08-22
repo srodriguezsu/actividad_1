9ca41b7dc028fa6538062d86bd108e9baff0ad74
srodriguezsu <srodriguezsu@unal.edu.co>
2023-08-22 17:50:43 -0500

    Exercise 14 improved with Math.pow

--- actividad_1/ejercicio_14/src/Main.java
+++ actividad_1/ejercicio_14/src/Main.java
@@ -4,20 +4,13 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
 
-    static float cuadrado( float a){
-        return(a * a);
-    }
-
-    static float cubo(float a){
-        return (a * a *a);
-    }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Inserte el número a evaluar: ");
         float numero = input.nextFloat();
-        System.out.println(numero + " al cuadrado es " + cuadrado(numero));
-        System.out.println(numero + " al cubo es " + cubo(numero));
+        System.out.println(numero + " al cuadrado es " + Math.pow(numero, 2));
+        System.out.println(numero + " al cubo es " + Math.pow(numero, 3));
 
     }
 }
